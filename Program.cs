77a848301fc73cedce21785a1ec8c61431d18251
relaxday123 <91047548+relaxday123@users.noreply.github.com>
2022-06-09 20:53:29 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace b5
+{
+    class Program
+    {
+        public static async Task PrintPrimeNumber(int startNumber, int endNumber)
+        {
+            await Task.Run(() =>
+            {
+                int ctr;
+
+                for (int i = startNumber; i <= endNumber; i++)
+                {
+                    ctr = 0;
+                    for (int j = 2; j <= i / 2; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            ctr++;
+                            break;
+                        }
+                    }
+
+                    if (ctr == 0 && i > 1)
+                    {
+                        Console.Write("{0} ", i);
+                        Task.Delay(100).Wait();
+                    }
+                }
+            });
+        }
+
+        public static void Main(string[] args)
+        {
+            PrintPrimeNumber(0, 100);
+            PrintPrimeNumber(101, 200);
+            Console.ReadKey();
+        }
+    }
+}
